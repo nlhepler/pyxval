b897ecf3fb6e0345ec2840150b008f9275ee4cb4
Lance Hepler <nlhepler@gmail.com>
2011-08-19 10:54:49 -0700

    Updated the URL in setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name='pyxval',
       description='Machine learning utilities',
       author='N Lance Hepler',
       author_email='nlhepler@gmail.com',
+      url='http://github.com/nlhepler/pyxval',
       license='GNU GPL version 3',
       packages=['pyxval']
      )
