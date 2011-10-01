1ffe6b1d2282e0c9b84ee331aa461683da4231c3
Brent Payne <brent.payne@brightscope.com>
2011-10-01 16:47:16 -0700

    added append to BaseScorer with documentation

--- pyxval/_basescorer.py
+++ pyxval/_basescorer.py
@@ -5,3 +5,12 @@ class BaseScorer(object):
 
     def __init__(self):
         pass
+
+    def append(self, truth, preds, weights=None):
+        '''
+        This adds provides a list of truth, predictions, and weights to the scorer
+        :param truth: the expected truth
+        :param preds: the predicted values
+        :param weights: optionally the weights associated with the observation that produced the prediction
+        '''
+        raise NotImplementedError
